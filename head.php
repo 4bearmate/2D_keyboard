bc886cfcfb49176615f2b61336000fd0b747a4d3
jinjoo <117711383+4bearmate@users.noreply.github.com>
2025-02-25 16:40:14 +0900

    add_javascript
    가상 키보드 관련 CDN

---
+++ head.php
@@ -0,0 +1,13 @@
+<head>
+<?php
+//가상 키보드 css
+add_stylesheet('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/virtual-keyboard/1.30.3/css/keyboard.min.css">', 0);
+
+//가상 키보드 CDN
+add_javascript('<script src="https://cdn.jsdelivr.net/gh/e-/Hangul.js/hangul.min.js"></script>', 0);
+add_javascript('<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>', 0);
+add_javascript('<script src="https://cdnjs.cloudflare.com/ajax/libs/virtual-keyboard/1.30.3/js/jquery.keyboard.min.js"></script>', 0);
+add_javascript('<script src="https://cdnjs.cloudflare.com/ajax/libs/virtual-keyboard/1.30.3/js/jquery.keyboard.extension-all.min.js"></script>', 0);
+
+?>
+<head>
